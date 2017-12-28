1ef672556bf684f546cf5d43ae4bed3b016b942f
Jazpy <jaz.medtre@gmail.com>
2017-12-28 12:17:26 -0600

    Added seq-gen interfacing

--- msprime_sim/gen_genomes.py
+++ msprime_sim/gen_genomes.py
@@ -1,4 +1,5 @@
 import msprime
+import subprocess
 
 # Specify our samples, 11 modern genomes and
 # 5 ancient genomes taken 25, 50, 100, 200, and
@@ -25,10 +26,23 @@ samples = [
         msprime.Sample(0, 400)
         ]
 
-# Run simulation and extract results
-#tree_seq = msprime.simulate(recombination_rate=2e-8, mutation_rate=2e-8,
-        #Ne=1e4, length=46e6, samples=samples)
-tree_seq = msprime.simulate(recombination_rate=2e-8, mutation_rate=2e-8,
-        Ne=1e4, length=500, samples=samples)
+# Number of bases to simulate
+#num_bases = 46e6
+num_bases = 10
 
-# Extract sequence data
+# Run simulation and extract results
+tree_seq = msprime.simulate(recombination_rate=2e-8, mutation_rate=2e-8,
+        Ne=1e4, length=num_bases, samples=samples)
+
+# Dump data for seq-gen compatibility
+tree_seq.dump('tree_data')
+
+# Get Newick format tree
+newick_filepath = 'newick_tree'
+newick_file = open(newick_filepath, 'w')
+subprocess.run(['msp', 'newick', 'tree_data'], stdout=newick_file)
+
+# Run seq-gen on Newick tree
+seqgen_filepath = 'sequence_data'
+seqgen_file = open(seqgen_filepath, 'w')
+subprocess.run(['seq-gen', '-mHKY', '-l' + str(num_bases), newick_filepath], stdout=seqgen_file)
